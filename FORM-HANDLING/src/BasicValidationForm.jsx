43c15d7b5d83a33b0de877ed830578f632bc5906
AKSHAY KUMAR <108133360+akshaykumar2043@users.noreply.github.com>
2026-05-10 00:42:20 +0530

    add new content

--- FORM-HANDLING/src/BasicValidationForm.jsx
+++ FORM-HANDLING/src/BasicValidationForm.jsx
@@ -1,13 +1,44 @@
-function BasicValidationForm(){
+import { useState } from "react";
+function BasicValidationForm() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
 
-return(
-<>
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        if (!name || !email) {
+            setError("please fill all fields!");
+        } else {
+            setError("");
+            console.log("From Submitted:", { name, email });
+            alert("From Submitted Successfully!");
+        }
+    };
 
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Basic Validation Form</h2>
 
-</>
-)
+            <input
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <br />
 
+            <input
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
 
-export default BasicValidationForm
+export default BasicValidationForm;
