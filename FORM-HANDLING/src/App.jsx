b6fbd0ce047eac4b1d5d6368373681f0c050d430
AKSHAY KUMAR <108133360+akshaykumar2043@users.noreply.github.com>
2026-05-10 00:23:15 +0530

    add new content

--- FORM-HANDLING/src/App.jsx
+++ FORM-HANDLING/src/App.jsx
@@ -2,12 +2,13 @@ import SimpleForm from "./Simpleform"
 import MultiInputFieldForm from "./MultiInputFieldForm"
 import AdvancedForm from "./AdvancedFrom"
 import UncontrolledForm from"./UncontrolledForm"
-
+import BasicValidationForm from"./BasicValidationForm"
 function App() {
  
 
   return (
     <>
+    <BasicValidationForm/>
     <UncontrolledForm/>
     <AdvancedForm/>
     <MultiInputFieldForm/>
